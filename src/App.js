00d159899e30420f7f082ae76a14d1d944526745
jasminecao <jasminecao03@gmail.com>
2020-05-26 14:47:34 -0400

    add router

--- src/App.js
+++ src/App.js
@@ -4,9 +4,11 @@ import logo from './logo.svg';
 import './App.css';
 import './w3school.css';
 import Menu from './components/MenuComponent';
+import {BrowserRouter as Router, Switch, Route, } from 'react-router-dom';
+import Upload from './components/Upload';
+import Home from './components/Home';
 
 import { CONFIG } from './shared/config';
-
 
 class App extends Component {
   constructor(props) {
@@ -17,28 +19,14 @@ class App extends Component {
   }
 render(){
   return (
-    <div className="App">
-      
-     <div class ="row row-header">
-      <div class="col-sm-1">
-      <img clas="img-responsive" src="assets/images/pro1.png" width = "50%"  ></img></div>
-      <div class="col-sm-2"> <a href='/index.html'  class='title-font' title='Home'>home</a></div>
-      <div class="col-sm-2"> <a href='/index.html' class='title-font' title='List'>group</a>
-      </div>
-      <div class="col-sm-2"> <a href='/user.html' class='title-font' title='List'>user</a>
-      </div>
-      <div class="col-sm-2"> <a href='/upload.html' class='title-font' title='List'>list</a>
-      </div>
-      <div class="col-sm-3"> <a href='/setting.html' class='title-font' title='List'>setting</a>
-      </div>
-
-      </div>
-      <Menu dishes={this.state.dishes} />
-      <div class ="footer fixed-bottom">
-      this is footer 
-      </div>
-      </div>
-    
+    <>
+    <Router>
+      <Switch>
+        <Route path="/" component={Home} exact />
+        <Route path="/upload" component={Upload}/>
+      </Switch>
+    </Router>
+    </>
   );
   }
 }
