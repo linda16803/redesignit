4d59828219f66c1257b2602a758d9467d64d9b07
jasminecao <jasminecao03@gmail.com>
2020-05-26 14:47:49 -0400

    change cards

--- src/components/MenuComponent.js
+++ src/components/MenuComponent.js
@@ -1,52 +1,48 @@
-import React, { Component } from 'react';
+import React, { Component, useState } from 'react';
 import { Card, CardImg, CardImgOverlay, CardTitle } from 'reactstrap';
 import DishDetail from './DishDetailComponents'
 import CardFooter from 'reactstrap/lib/CardFooter';
+import {BrowserRouter as Router, Switch, Route, Link, Redirect} from 'react-router-dom';
 
-class Menu extends Component {
+const Menu = props => {
+  const {dishes} = props;
+  const [selectedDish, setSelectedDish] = useState(null);
+  const [dishLink, setDishLink] = useState(null);
 
-    constructor(props) {
-        super(props);
+  function onSelectedDish(dish) {
+    setSelectedDish(dish);
+  }
 
-        this.state = {
-            selectedDish: null
-        }
-    }
+  function leaveDishCard(dish) {
+    setSelectedDish(null);
+  }
 
-    onSelectedDish(dish) {
-        this.setState({
-            selectedDish: dish
-        })
-    }
+  function changeDishName(name) {
+    setDishLink(name);
+  }
 
-    render() {
-        const menu = this.props.dishes.map((dish) => {
-          return (
-            <div key={dish.id} className='col-2 spacer5 '>
-                 <Card className="text-center" onClick={() => this.onSelectedDish(dish)} >
-                 
-                       <CardImg className="img" width="50%" src={dish.image} alt={dish.name} />
-                       
-                    <CardTitle className="backgroundcolor">{dish.name}</CardTitle>
-                    
-                </Card>
-                
-                </div>
-        );
-       });
-        return (
-            <div className="container">
-                <div className="row">
-                <div className='col-12 with-margin'></div>
-                </div>
-            
-                <div className="row">
-                    {menu}
-                </div>
-                <DishDetail dish={this.state.selectedDish}></DishDetail>
-            </div>
-        );
-    }
+  return (
+      <div className="container">
+        <div className="row">
+          <div className='col-12 with-margin'></div>
+        </div>
+        <div className="row">
+        {dishes.map((dish) => (
+        <div key={dish.id} className='col-2 spacer5 '>
+          <Card className="text-center" 
+            onMouseEnter={() => onSelectedDish(dish)} 
+            onMouseLeave={() => leaveDishCard(dish)}
+            onClick={() => changeDishName(dish.name)}>
+            {dishLink === "Upload" && <Redirect to="/upload">{dish.name}</Redirect>}
+            <CardImg className="img" width="50%" src={dish.image} alt={dish.name} />
+            <CardTitle className="backgroundcolor">{dish.name}</CardTitle>
+          </Card>
+        </div>
+      ))}
+      </div>
+        <DishDetail dish={selectedDish} style={{height: "50%", width: "50%"}}></DishDetail>
+      </div>
+    );
 }
 
 export default Menu; 
